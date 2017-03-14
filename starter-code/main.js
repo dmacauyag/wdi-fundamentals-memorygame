65d1a848168fddccc22e272829d888cbf1634296
dmacauyag <dmacauyag@Daniels-MacBook-Air.local>
2017-03-14 14:14:26 -0700

    implemented conditional statements to check for matching cards using alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,14 @@
-// output tests that JS file is linked to the index.html
-console.log("JS file is connected to HTML! Woo!")
-
 // variables that simulate the cards in the game as either a "king" or "queen"
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+// check for equality between two cards and display alert in the browser
+if (cardOne === cardTwo) {
+	alert("You found a match!");
+} else if (cardThree === cardFour) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.")
+}
