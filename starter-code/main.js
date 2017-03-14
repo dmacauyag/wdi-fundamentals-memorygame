bb995a2593264c10cb278e5d8b89037dbc76e90e
dmacauyag <dmacauyag@Daniels-MacBook-Air.local>
2017-03-13 21:33:28 -0700

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,8 @@
+// output tests that JS file is linked to the index.html
 console.log("JS file is connected to HTML! Woo!")
+
+// variables that simulate the cards in the game as either a "king" or "queen"
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
