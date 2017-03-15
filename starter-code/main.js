b6a53b80e1d3ed9b52c8c02ca9b03818b0429d3e
dmacauyag <dmacauyag@Daniels-MacBook-Air.local>
2017-03-15 11:41:47 -0700

    Unit 10 Assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,10 +5,26 @@ var cardThree = "king";
 var cardFour = "king";
 
 // check for equality between two cards and display alert in the browser
-if (cardOne === cardTwo) {
+/* if (cardOne === cardTwo) {
 	alert("You found a match!");
 } else if (cardThree === cardFour) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.")
-}
+} */
+
+// selects elements that have an id of "game-board"
+var gameBoard = document.getElementById('game-board');
+
+// function to create cards for the memory game
+var createCards = function() {
+	// for loop that creates an HTML div element for each card, assuming 4 cards
+	for (var i = 0; i < 4; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+	}
+};
+
+// call function to create the cards for the game
+createCards();
